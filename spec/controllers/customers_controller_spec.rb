506d2e0ff2c0a52b98ff519a82f0402e8b48b2de
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2018-09-21 08:35:54 -0300

    POST customers returns the model created

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe CustomersController, type: :controller do
   context 'when saving a user' do
-    it 'saves in the database' do
+    it 'returns 201' do
       post :create, body: JSON.dump(name: 'maria da silva')
 
       expect(response).to have_http_status(201)
-      expect(Customer.last.name).to eq 'maria da silva'
+    end
+
+    it 'returns the customer created' do
+      post :create, body: JSON.dump(name: 'maria da silva')
+
+      response_body = JSON.parse(response.body)
+      expect(response_body['id']).not_to be_nil
+      expect(response_body['name']).to eq 'maria da silva'
     end
   end
 end
